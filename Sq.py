04d682ae9291d92bc5876eefee9307d144c498bc
Alexander Sage <SillySage@github.com>
2018-10-13 04:32:44 +0500

    Треугольник

---
+++ Sq.py
@@ -0,0 +1,5 @@
+n= int(input("n"))
+for i in range(0,n,1):
+    for i in range(0,n,1):
+        print("*",end ="")
+    print()
---
+++ Tri.py
@@ -0,0 +1,5 @@
+n= int(input("Enter n "))
+for a in range(0,n,1):
+    for b in range(0,a+1,1):
+        print ("*", end='')
+    print ()
